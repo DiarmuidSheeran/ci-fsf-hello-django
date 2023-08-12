47e8dd74ac647b9883393b627b1e240620e8fa05
Diarmuid Sheeran <dssheeran@outlook.com>
2023-08-12 15:51:14 +0000

    migrate code

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4_e3f5i#fh-pct!3!=oae^w28qfpi$ua8z0le#tsqlehakq=h6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sam2008-django-todo-app-3734efadaf0f.herokuapp.com']
+ALLOWED_HOSTS = ['sam2008-django-todo-app-3734efadaf0f.herokuapp.com', '8000-diarmuidshe-cifsfhellod-26bpqasq7th.ws-eu103.gitpod.io']
 
 
 # Application definition
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #   }
 #}
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('postgres://nlfenzoi:Ry0C4IOQCKzLyNtASJdhU31ZehwxYQFV@trumpet.db.elephantsql.com/nlfenzoi'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
